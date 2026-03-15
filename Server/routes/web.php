02681a4edf23f99ca0db8b66fb5e68b80669abc0
Erik C <erik.conesa@gmail.com>
2026-03-15 19:37:37 +0100

    Configuración inicial de modelos

--- Server/routes/web.php
+++ Server/routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function () {return response()->json(['message' => 'API works!', 'test' => 'Another entry'], 200);;});
+Route::get('api/users', [UserController::class, 'index']);
+Route::get('api/users/{id}', [UserController::class, 'show']);
+Route::post('api/users', [UserController::class, 'store']);
+Route::put('api/users/{id}', [UserController::class, 'update']);
+Route::delete('api/users/{id}', [UserController::class, 'destroy']);
+
+
+
+
+
+
