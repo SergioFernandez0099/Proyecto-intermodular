6e4a41975d104fddf98ea270fc8502a47ba9719b
Erik C <erik.conesa@gmail.com>
2026-04-11 18:12:37 +0200

    Actualizar servicios para que usen modelos

--- Front/src/app/services/book.ts
+++ Front/src/app/services/book.ts
@@ -2,19 +2,11 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { IBook } from '../models/book';
 
-export interface Book {
-  id: number;
-  title: string;
-  cover_image: string;
-  publication_year: number;
-  genre: string;
-  authors_list: string[];
-  status?: string; 
-}
 
 export interface BookResponse {
-  data: Book[];
+  data: IBook[];
 }
 
 @Injectable({
@@ -24,7 +16,7 @@ export class BookService {
   private apiUrl = 'http://localhost:8000/api/books'; 
   constructor(private http: HttpClient) {}
 
-  getBooks(): Observable<Book[]> {
+  getBooks(): Observable<IBook[]> {
     return this.http.get<BookResponse>(this.apiUrl).pipe(
       map(response => response.data)
     );
--- Front/src/app/services/user.ts
+++ Front/src/app/services/user.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { IUser } from '../models/user';
 
 @Injectable({
   providedIn: 'root'
@@ -15,13 +16,13 @@ export class UserService {
     return this.http.post(this.apiLogin, credentials);
   }
   
-  getUsers(): Observable<any[]> {
-    return this.http.get<any[]>(this.apiUrl);
+  getUsers(): Observable<IUser[]> {
+    return this.http.get<IUser[]>(this.apiUrl);
   }
 
   
-  getUserById(id: number | string): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}/${id}`);
+  getUserById(id: number | string): Observable<IUser> {
+    return this.http.get<IUser>(`${this.apiUrl}/${id}`);
   }
 
   createUser(userData: any): Observable<any> {
@@ -29,11 +30,11 @@ export class UserService {
   }
 
   
-  updateUser(id: number | string, userData: any): Observable<any> {
-    return this.http.put<any>(`${this.apiUrl}/${id}`, userData);
+  updateUser(id: number | string, userData: IUser): Observable<IUser> {
+    return this.http.put<IUser>(`${this.apiUrl}/${id}`, userData);
   }
 
-  deleteUser(id: number | string): Observable<any> {
-    return this.http.delete<any>(`${this.apiUrl}/${id}`);
+  deleteUser(id: number | string): Observable<IUser> {
+    return this.http.delete<IUser>(`${this.apiUrl}/${id}`);
   }
 }
