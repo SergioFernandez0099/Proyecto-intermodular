522ef5e9d445f5f2848a976c35393f0cd5ebe696
Erik C <erik.conesa@gmail.com>
2026-05-08 16:50:58 +0200

    Ajuste produccion

--- Front/src/app/core/constants/api.ts
+++ Front/src/app/core/constants/api.ts
@@ -1,3 +1,5 @@
- export const SERVER_BASE = 'http://localhost:8000';
-//export const SERVER_BASE = 'https://tfg.sergiof.es';
+import { isInProduction } from "./production";
+
+export const SERVER_BASE = (isInProduction) ? 
+  'https://tfg.sergiof.es' : 'http://localhost:8000';
 export const API_BASE = `${SERVER_BASE}/api`;
